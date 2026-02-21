b07932357119ab0931a24fa6a2f36135c67487ee
Aravind <gmarav005@gmail.com>
2026-02-21 18:25:41 +0530

    Add Query provider and update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import connectDB from "@/lib/db.js"
 
-export default function Home() {
+export default async function Home() {
+
+  const connection = await connectDB();
+  console.log(connection)
+
   return (
     <Button>
       Welcome to Todo App
