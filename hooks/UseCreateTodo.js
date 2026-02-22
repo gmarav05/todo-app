674fd6c7874b69bf4b92ab58bc074b843e519574
Aravind <gmarav005@gmail.com>
2026-02-22 18:46:33 +0530

    Update UseCreateTodo

--- hooks/UseCreateTodo.js
+++ hooks/UseCreateTodo.js
@@ -1,6 +1,27 @@
 import { createTodo } from "@/actions/todo-actions"
 import { createTodoSchema } from "@/validations/todo"
+import { useTodoStore } from "@/store/TodoStore";
+import { mutationOptions, useMutation, useQueryClient } from "@tanstack/react-query";
 
-export function UseTodos() {
-    const setTodos = useTodoStore((state) => state.setTodo);
+export const todoKeys = {
+    all:["todos"],
+    lists:()=>[...todoKeys.all , "list"]
+}
+
+export function UseCreateTodo() {
+    const queryClient = useQueryClient;
+    const addTodo = useTodoStore((state) => state.setTodo);
+
+    return useMutation({
+        mutationFn:(data)=> createTodo(data),
+
+        onSuccess:(result)=> {
+            if(result.success) {
+                addTodo(result.data)
+
+                queryClient.invalidateQueries({queryKey:todoKeys.list()})
+            }
+        }
+
+    }) 
 }
